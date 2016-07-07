96008a55c57b73f5363b8b13b25c09fba7274e44
dmitrybublik <dmitry.buba@gmail.com>
2016-07-07 12:38:04 +0300

    Added small template solution.

---
+++ templates/LogoFX.Templates/LogoFX.Templates.Client.Shell/ViewModels/ShellViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using Caliburn.Micro;
+using JetBrains.Annotations;
+
+namespace LogoFX.Templates.Client.Shell.ViewModels
+{
+    [UsedImplicitly]
+    public class ShellViewModel : Conductor<INotifyPropertyChanged>.Collection.OneActive
+    {
+
+    }
+}
