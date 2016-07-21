aac585c117fd50d46a3e425f330188a557b3605e
dmitrybublik <dmitry.buba@gmail.com>
2016-07-21 21:49:12 +0300

    Added Client.Data.Fake projects.

---
+++ src/LogoFX.Tools.Templates/ProjectTemplates/Samples.Specifications/Client/Data/Fake/ProviderBuildersSetup/Module.cs
@@ -0,0 +1,17 @@
+﻿using Attest.Fake.Core;
+using Attest.Fake.Moq;
+using JetBrains.Annotations;
+using Solid.Practices.Modularity;
+
+namespace $safeprojectname$
+{    
+    [UsedImplicitly]    
+    class Module : IPlainCompositionModule
+    {
+        public void RegisterModule()
+        {
+            FakeFactoryContext.Current = new FakeFactory();
+            ConstraintFactoryContext.Current = new ConstraintFactory();
+        }
+    }
+}
---
+++ src/LogoFX.Tools.Templates/ProjectTemplates/Samples.Specifications/Client/Data/Fake/Providers/FakeWarehouseProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Attest.Fake.Builders;
+using JetBrains.Annotations;
+using Samples.Client.Data.Contracts.Dto;
+using Samples.Client.Data.Contracts.Providers;
+using Samples.Specifications.Client.Data.Fake.Containers;
+using Samples.Specifications.Client.Data.Fake.ProviderBuilders;
+using Solid.Practices.Scheduling;
+
+namespace $safeprojectname$
+{
+    [UsedImplicitly]
+    class FakeWarehouseProvider : FakeProviderBase<WarehouseProviderBuilder, IWarehouseProvider>, IWarehouseProvider
+    {
+        private readonly WarehouseProviderBuilder _warehouseProviderBuilder;
+        private readonly Random _random = new Random();
+
+        public FakeWarehouseProvider(
+            WarehouseProviderBuilder warehouseProviderBuilder,
+            IWarehouseContainer warehouseContainer)
+        {
+            _warehouseProviderBuilder = warehouseProviderBuilder;
+            _warehouseProviderBuilder.WithWarehouseItems(warehouseContainer.WarehouseItems);
+        }
+
+        async Task<IEnumerable<WarehouseItemDto>> IWarehouseProvider.GetWarehouseItems()
+        {
+            await TaskRunner.RunAsync(() => Thread.Sleep(_random.Next(2000)));
+            var service = GetService(() => _warehouseProviderBuilder, b => b);
+            var warehouseItems = await service.GetWarehouseItems();
+            return warehouseItems;
+        }
+    }
+}
