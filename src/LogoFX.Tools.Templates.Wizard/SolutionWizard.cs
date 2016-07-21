be3ca0c63735b3b1aba3bd5775d11279579088dd
dmitrybublik <dmitry.buba@gmail.com>
2016-07-21 14:21:42 +0300

    Added custom Wizard.

---
+++ src/LogoFX.Tools.Templates.Wizard/SolutionWizard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+
+namespace LogoFX.Tools.Templates.Wizard
+{
+    public class SolutionWizard : IWizard
+    {
+        private readonly TemplateBuilder.SolutionWizard _solutionWizard = 
+            new TemplateBuilder.SolutionWizard();
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            var form = new UserInputForm();
+            form.ShowDialog();
+
+            _solutionWizard.RunStarted(automationObject, replacementsDictionary, runKind, customParams);
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            return _solutionWizard.ShouldAddProjectItem(filePath);
+        }
+
+        public void RunFinished()
+        {
+            _solutionWizard.RunFinished();
+        }
+
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+            _solutionWizard.BeforeOpeningFile(projectItem);
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+            _solutionWizard.ProjectItemFinishedGenerating(projectItem);
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+            _solutionWizard.ProjectFinishedGenerating(project);
+        }
+    }
+}
---
+++ src/LogoFX.Tools.Templates.Wizard/UserInputForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogoFX.Tools.Templates.Wizard
+{
+    public partial class UserInputForm : Form
+    {
+        public UserInputForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
