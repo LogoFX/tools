59efdaaf2bc5bde6cc1c7857a3618456cf8a4ef8
dmitrybublik <dmitry.buba@gmail.com>
2016-07-21 20:36:28 +0300

    Added Client.Model.Shared project.

---
+++ src/LogoFX.Tools.Templates/ProjectTemplates/Samples.Specifications/Client/Model/Shared/UserContext.cs
@@ -0,0 +1,23 @@
+﻿using Samples.Client.Model.Contracts;
+
+namespace $safeprojectname$
+{
+    public static class UserContext
+    {
+        private static IUser _currentUser;
+
+        public static IUser Current
+        {
+            get { return _currentUser; }
+            set
+            {
+                if (_currentUser == value)
+                {
+                    return;
+                }
+
+                _currentUser = value;         
+            }
+        }        
+    }
+}
