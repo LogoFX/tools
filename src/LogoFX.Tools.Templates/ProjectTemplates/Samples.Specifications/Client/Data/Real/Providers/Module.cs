ed03de883753bf51d1fb2e902aed00cea9052a22
dmitrybublik <dmitry.buba@gmail.com>
2016-07-22 12:39:01 +0300

    Added Client.Data.Real.Providers project

---
+++ src/LogoFX.Tools.Templates/ProjectTemplates/Samples.Specifications/Client/Data/Real/Providers/Module.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using Samples.Client.Data.Contracts.Providers;
+using Solid.Practices.IoC;
+using Solid.Practices.Modularity;
+
+namespace $safeprojectname$
+{    
+    [UsedImplicitly]
+    class Module : ICompositionModule<IIocContainerRegistrator>
+    {
+        public void RegisterModule(IIocContainerRegistrator iocContainer)
+        {
+            iocContainer.RegisterSingleton<IWarehouseProvider, WarehouseProvider>();
+        }
+    }
+}
---
+++ src/LogoFX.Tools.Templates/ProjectTemplates/Samples.Specifications/Client/Data/Real/Providers/WarehouseProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Samples.Client.Data.Contracts.Dto;
+using Samples.Client.Data.Contracts.Providers;
+
+namespace $safeprojectname$
+{
+    class WarehouseProvider : IWarehouseProvider
+    {
+        public Task<IEnumerable<WarehouseItemDto>> GetWarehouseItems()
+        {
+            //put here real data logic
+            return Task.FromResult((IEnumerable<WarehouseItemDto>) new[]
+            {
+                new WarehouseItemDto
+                {
+                    Kind = "Acme",
+                    Price = 10,
+                    Quantity = 10
+                }
+            });           
+        }
+    }
+}
