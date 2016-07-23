51dfd002883e389f686663799a35970a2828087c
dmitrybublik <dmitry.buba@gmail.com>
2016-07-23 15:26:26 +0300

    Added Tests.Acceptance.Infra.Providers and Tests.Acceptance.Infra.Real project template.

---
+++ src/LogoFX.Tools.Templates/ProjectTemplates/Samples.Specifications/Tests/Acceptance/Infra/Real/Module.cs
@@ -0,0 +1,15 @@
+﻿using LogoFX.Client.Testing.Contracts;
+using LogoFX.Client.Testing.EndToEnd.White;
+using Solid.Practices.IoC;
+using Solid.Practices.Modularity;
+
+namespace $safeprojectname$
+{
+    class Module : ICompositionModule<IIocContainerRegistrator>
+    {
+        public void RegisterModule(IIocContainerRegistrator iocContainer)
+        {
+            iocContainer.RegisterInstance<IStartApplicationService>(new StartApplicationService.WithRealProviders());
+        }
+    }
+}
