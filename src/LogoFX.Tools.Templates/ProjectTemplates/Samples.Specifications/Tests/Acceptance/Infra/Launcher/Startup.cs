f6ca8d63fa735d0561e1804c7bdc074936b71f5b
dmitrybublik <dmitry.buba@gmail.com>
2016-07-23 15:01:36 +0300

    Added Tests.Acceptance.Infra.Launcher project template.

---
+++ src/LogoFX.Tools.Templates/ProjectTemplates/Samples.Specifications/Tests/Acceptance/Infra/Launcher/Startup.cs
@@ -0,0 +1,24 @@
+﻿using BoDi;
+using LogoFX.Bootstrapping;
+using LogoFX.Client.Testing.EndToEnd.SpecFlow;
+using TechTalk.SpecFlow;
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// Represents SpecFlow bridge which performs the required registrations
+    /// for screen objects and application services
+    /// </summary>
+    [Binding]
+    class Startup : EndToEndTestsBase
+    {
+        public Startup(IObjectContainer objectContainer)
+        {
+            var containerAdapter = new ObjectContainerAdapter(objectContainer);
+            var bootstrapper =
+                new Bootstrapper(containerAdapter)
+                .Use(new RegisterCompositionModulesMiddleware<Bootstrapper>());            
+            bootstrapper.Initialize();            
+        }                       
+    }
+}
