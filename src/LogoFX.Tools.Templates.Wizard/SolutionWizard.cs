646655747ac40c0a449b1971a168045f0eef7317
dmitrybublik <dmitry.buba@gmail.com>
2016-10-02 02:13:44 +0300

    Fixed solution creation

--- src/LogoFX.Tools.Templates.Wizard/SolutionWizard.cs
+++ src/LogoFX.Tools.Templates.Wizard/SolutionWizard.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -41,44 +42,40 @@ namespace LogoFX.Tools.Templates.Wizard
 
         #region Private Members
 
-        private void RemoveConditions()
+        private void RemoveConditions(IEnumerable<Project> projects)
         {
-            var projects = GetProjects();
             foreach (var project in projects)
             {
                 RemoveConditions(project);
             }
         }
 
-        private void ApplyWizardModifications()
+        private IEnumerable<Project> ApplyWizardModifications(IEnumerable<Project> projects)
         {
             if (_wizardViewModel == null)
             {
-                return;
+                return projects;
             }
 
-            if (_wizardViewModel.SelectedSolution != null)
-            {
-                RemoveMultiSolution(_wizardViewModel.SelectedSolution.Model);
-            }
+            return RemoveMultiSolution(_wizardViewModel.SelectedSolution.Model);
 
-            if (!_wizardViewModel.CreateTests)
-            {
-                RemoveTestProjects();
-            }
+            //if (!_wizardViewModel.CreateTests)
+            //{
+            //    RemoveTestProjects();
+            //}
 
-            if (!_wizardViewModel.CreateFakes)
-            {
-                RemoveFakesProjects();
-            }
+            //if (!_wizardViewModel.CreateFakes)
+            //{
+            //    RemoveFakesProjects(projects);
+            //}
 
-            if (!_wizardViewModel.CreateTests || !_wizardViewModel.CreateFakes)
-            {
-                RemoveConditions();
-            }
+            //if (!_wizardViewModel.CreateTests || !_wizardViewModel.CreateFakes)
+            //{
+            //    RemoveConditions(projects);
+            //}
         }
 
-        private void RemoveMultiSolution(SolutionInfoDto solutionInfo)
+        private IEnumerable<Project> RemoveMultiSolution(SolutionInfoDto solutionInfo)
         {
             SolutionFolderTemplate selected = null;
             foreach (var p in _solution.Projects.OfType<Project>().ToList())
@@ -91,26 +88,30 @@ namespace LogoFX.Tools.Templates.Wizard
                 _solution.Remove(p);
             }
 
+            List<Project> projects = new List<Project>();
+
             Debug.Assert(selected != null, "selected != null");
             foreach (var p in selected.Items)
             {
-                AddProjectToSolution(null, p);
+                AddProjectToSolution(null, p, projects);
             }
+
+            return projects;
         }
 
-        private void AddProjectToSolution(SolutionFolder parent, SolutionItemTemplate project)
+        private void AddProjectToSolution(SolutionFolder parent, SolutionItemTemplate project, IList<Project> projects)
         {
             if (project is SolutionFolderTemplate)
             {
-                AddSolutionFolder(parent, (SolutionFolderTemplate)project);
+                AddSolutionFolder(parent, (SolutionFolderTemplate)project, projects);
             }
             else
             {
-                AddProject(parent, (ProjectTemplate)project);
+                AddProject(parent, (ProjectTemplate)project, projects);
             }
         }
 
-        private void AddSolutionFolder(SolutionFolder parent, SolutionFolderTemplate solutionFolder)
+        private void AddSolutionFolder(SolutionFolder parent, SolutionFolderTemplate solutionFolder, IList<Project> projects)
         {
             var addedProject = parent == null
                 ? _solution.AddSolutionFolder(solutionFolder.Name)
@@ -118,15 +119,105 @@ namespace LogoFX.Tools.Templates.Wizard
 
             foreach (var item in solutionFolder.Items)
             {
-                AddProjectToSolution(addedProject.Object as SolutionFolder, item);
+                AddProjectToSolution(addedProject.Object as SolutionFolder, item, projects);
             }
         }
 
-        private void AddProject(SolutionFolder parent, ProjectTemplate project)
+        private void AddProject(SolutionFolder parent, ProjectTemplate project, IList<Project> projects)
         {
+            var projectFullName = project.FileName;
+            var projectFileName = Path.GetFileName(projectFullName);
+            var projectDirectory = Path.GetDirectoryName(projectFullName);
+            var newProjectDirectory = Path.Combine(
+                Path.GetDirectoryName(
+                    Path.GetDirectoryName(projectDirectory)),
+                Path.GetFileName(projectDirectory));
+            var newProjectFullName = Path.Combine(newProjectDirectory, projectFileName);
+
+            CopyDirectory(projectDirectory, newProjectDirectory);
+            //DeleteDirectory(projectDirectory);
+
             var addedProject = parent == null
-                ? _solution.AddFromFile(project.FileName)
-                : parent.AddFromFile(project.FileName);
+                ? _solution.AddFromFile(newProjectFullName)
+                : parent.AddFromFile(newProjectFullName);
+
+            projects.Add(addedProject);
+        }
+
+        /// <summary>
+        /// Copies the directory.
+        /// </summary>
+        /// <param name="sourceDirectory">The source directory.</param>
+        /// <param name="destDirectory">The destination directory.</param>
+        private static void CopyDirectory(string sourceDirectory, string destDirectory)
+        {
+            if (!Directory.Exists(destDirectory))
+            {
+                Directory.CreateDirectory(destDirectory);
+            }
+
+            var files = Directory.GetFiles(sourceDirectory);
+            foreach (var file in files)
+            {
+                var name = Path.GetFileName(file);
+                if (name == null) continue;
+
+                var dest = Path.Combine(destDirectory, name);
+                File.Copy(file, dest);
+            }
+
+            var folders = Directory.GetDirectories(sourceDirectory);
+            foreach (var folder in folders)
+            {
+                var name = Path.GetFileName(folder);
+                if (name == null) continue;
+
+                var dest = Path.Combine(destDirectory, name);
+                CopyDirectory(folder, dest);
+            }
+        }
+
+        /// <summary>
+        /// Deletes the directory.
+        /// </summary>
+        /// <param name="directory">The directory.</param>
+        /// <param name="inUseRetryCount">The in use retry count.</param>
+        private static void DeleteDirectory(string directory, int inUseRetryCount = 3)
+        {
+            var counter = 0;
+            while (counter < inUseRetryCount)
+            {
+                try
+                {
+                    if (!Directory.Exists(directory))
+                    {
+                        return;
+                    }
+
+                    var files = Directory.GetFiles(directory);
+                    foreach (var file in files)
+                    {
+                        File.Delete(file);
+                    }
+
+                    var folders = Directory.GetDirectories(directory);
+                    foreach (var folder in folders)
+                    {
+                        var name = Path.GetFileName(folder);
+                        if (name == null) continue;
+
+                        DeleteDirectory(folder);
+                    }
+
+                    Directory.Delete(directory);
+                    break;
+                }
+                catch (Exception)
+                {
+                    System.Threading.Thread.Sleep(2000);
+                    counter++;
+                }
+            }
         }
 
         private void RemoveTestProjects()
@@ -140,18 +231,16 @@ namespace LogoFX.Tools.Templates.Wizard
             }
         }
 
-        private void RemoveFakesProjects()
+        private void RemoveFakesProjects(IEnumerable<Project> projects)
         {
-            var projects = GetProjects(true);
             foreach (var p in projects.Where(p => p.Name.Contains(".Fake.")))
             {
                 _solution.Remove(p);
             }
         }
 
-        private void SetStartupProject()
+        private void SetStartupProject(IEnumerable<Project> projects)
         {
-            var projects = GetProjects(true);
             var startupProject = projects.FirstOrDefault(x => x.Name.EndsWith("Launcher"));
             if (startupProject == null)
             {
@@ -296,8 +385,15 @@ namespace LogoFX.Tools.Templates.Wizard
 
         public void RunFinished()
         {
-            ApplyWizardModifications();
-            SetStartupProject();
+            //Get all projects in solution
+            IEnumerable<Project> projects = GetProjects().ToList();
+            if (projects == null || !projects.Any())
+            {
+                throw new Exception("No projects found.");
+            }
+
+            projects = ApplyWizardModifications(projects);
+            SetStartupProject(projects);
         }
 
         public void BeforeOpeningFile(ProjectItem projectItem)
