3df1870415c90a629fcc2b4aafbb21306ec01da1
dmitrybublik <dmitry.buba@gmail.com>
2016-10-15 00:33:48 +0300

    Added StartupProject selection.

---
+++ src/LogoFX.Tools.TemplateGenerator.Shell/ViewModels/ProjectViewModel.cs
@@ -0,0 +1,19 @@
+﻿using LogoFX.Client.Mvvm.ViewModel;
+using LogoFX.Tools.TemplateGenerator.Contracts;
+
+namespace LogoFX.Tools.TemplateGenerator.Shell.ViewModels
+{
+    public sealed class ProjectViewModel : ObjectViewModel<IProjectTemplateInfo>
+    {
+        public ProjectViewModel(IProjectTemplateInfo model)
+            : base(model)
+        {
+
+        }
+
+        public string Name
+        {
+            get { return Model.NameWithoutRoot; }
+        }
+    }
+}
--- src/LogoFX.Tools.TemplateGenerator.Shell/ViewModels/SolutionViewModel.cs
+++ src/LogoFX.Tools.TemplateGenerator.Shell/ViewModels/SolutionViewModel.cs
@@ -1,4 +1,10 @@
-﻿using LogoFX.Client.Mvvm.ViewModel;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using LogoFX.Client.Mvvm.Commanding;
+using LogoFX.Client.Mvvm.ViewModel;
 using LogoFX.Client.Mvvm.ViewModel.Contracts;
 using LogoFX.Tools.TemplateGenerator.Contracts;
 
@@ -10,6 +16,19 @@ namespace LogoFX.Tools.TemplateGenerator.Shell.ViewModels
             : base(model)
         {
             CreateSolutionTemplateInfo();
+        }
+
+        private ICommand _browseIconCommand;
+
+        public ICommand BrowseIconCommand
+        {
+            get
+            {
+                return _browseIconCommand ??
+                       (_browseIconCommand = ActionCommand
+                           .When(() => false)
+                           .Do(() => { }));
+            }
         }
 
         public ISolutionTemplateInfo SolutionTemplateInfo
@@ -27,6 +46,20 @@ namespace LogoFX.Tools.TemplateGenerator.Shell.ViewModels
             }
         }
 
+        private WrappingCollection.WithSelection _projects;
+        public WrappingCollection.WithSelection Projects
+        {
+            get { return _projects ?? (_projects = CreateProjects()); }
+        }
+
+        private WrappingCollection.WithSelection CreateProjects()
+        {
+            var result = new WrappingCollection.WithSelection();
+            result.FactoryMethod = o => new ProjectViewModel((IProjectTemplateInfo) o);
+            result.AddSource(GetPlainProjects(Model.SolutionTemplateInfo.Items));
+            return result;
+        }
+
         private async void CreateSolutionTemplateInfo()
         {
             IsBusy = true;
@@ -40,6 +73,32 @@ namespace LogoFX.Tools.TemplateGenerator.Shell.ViewModels
             {
                 IsBusy = false;
             }
+        }
+
+        private IEnumerable<IProjectTemplateInfo> GetPlainProjects(IEnumerable<ISolutionItemTemplateInfo> items)
+        {
+            var result = new List<IProjectTemplateInfo>();
+
+            foreach (var item in items)
+            {
+                var folder = item as ISolutionFolderTemplateInfo;
+                if (folder != null)
+                {
+                    result.AddRange(GetPlainProjects(folder.Items));
+                    continue;
+                }
+
+                var project = item as IProjectTemplateInfo;
+                if (project != null)
+                {
+                    result.Add(project);
+                    continue;
+                }
+
+                throw new ArgumentException($"Unknown Solution Item type '{item.GetType().Name}'.");
+            }
+
+            return result;
         }
     }
 }
