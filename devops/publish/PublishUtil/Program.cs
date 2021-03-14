07d1cf0ab39d437cbd41d19cc63ee120ed492344
Gennady Verdel <godrose@gmail.com>
2021-03-14 19:23:29 +0200

    devops: adding publish for publish.bat case #9

--- devops/publish/PublishUtil/Program.cs
+++ devops/publish/PublishUtil/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using Solid.Core;
 
 namespace PublishUtil
@@ -8,8 +11,47 @@ namespace PublishUtil
     {
         private static void Main(string[] args)
         {
+            GoUp(5);
             var single = args != null && args.Length > 0 ? args[0] : null;
-            var directories = InitTopology();
+            var packageGroups = InitTopology();
+            foreach (var packageGroup in packageGroups)
+            {
+                PublishGroup(packageGroup.Id);
+            }
+        }
+
+        private static void PublishGroup(string packageGroupId)
+        {
+            Directory.SetCurrentDirectory(Path.Combine(Directory.GetCurrentDirectory(), packageGroupId));
+            var rootDir = Directory.GetCurrentDirectory();
+            if (Directory.Exists(Path.Combine(rootDir, "devops")))
+            {
+                Directory.SetCurrentDirectory(Path.Combine(rootDir, "devops"));
+                if (Directory.Exists(Path.Combine(Directory.GetCurrentDirectory(), "publish")))
+                {
+                    Directory.SetCurrentDirectory(Path.Combine(Directory.GetCurrentDirectory(), "publish"));
+                    var publishBat = Path.Combine(Directory.GetCurrentDirectory(), "publish.bat");
+                    if (File.Exists(publishBat))
+                    {
+                        var process = Process.Start(publishBat, new [] { "../../../../packages/Tests-All" });
+                        process.WaitForExit();
+                    }
+
+                    GoUp(3);
+                    return;
+                }
+
+                GoUp(2);
+                return;
+            }
+            GoUp(1);
+        }
+
+        private static void GoUp(int numberOfLevels)
+        {
+            var relativePath = new List<string> { Directory.GetCurrentDirectory() };
+            relativePath.AddRange(Enumerable.Repeat("..", numberOfLevels));
+            Directory.SetCurrentDirectory(Path.Combine(relativePath.ToArray()));
         }
 
         private static IEnumerable<IPackageGroup> InitTopology()
