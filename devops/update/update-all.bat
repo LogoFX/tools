33de67fcfceb36258c3bc9a5a03383de5ecc4cde
Gennady Verdel <godrose@gmail.com>
2021-04-20 19:32:34 +0300

    chore: minor

--- devops/update/update-all.bat
+++ devops/update/update-all.bat
@@ -1,4 +1,4 @@
 call update-single bump-dependency-version Solid 2.3.0-rc1
-call update-single bump-dependency-version Attest 2.1.0-rc2
+call update-single bump-dependency-version Attest 2.1.0-rc3
 call update-single bump-version LogoFX 2.2.0-rc2
 call update-single bump-dependency-version LogoFX 2.2.0-rc2
