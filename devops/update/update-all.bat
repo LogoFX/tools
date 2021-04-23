cd2e317e2a1309b70c5d3595ef1b093787d563b5
Gennady Verdel <godrose@gmail.com>
2021-04-23 19:28:17 +0300

    chore: bump deps

--- devops/update/update-all.bat
+++ devops/update/update-all.bat
@@ -1,4 +1,4 @@
 call update-single bump-dependency-version Solid 2.3.0
-call update-single bump-dependency-version Attest 2.1.0-rc4
+call update-single bump-dependency-version Attest 2.1.0
 call update-single bump-version LogoFX 2.2.0-rc2
 call update-single bump-dependency-version LogoFX 2.2.0-rc2
