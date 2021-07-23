cc9c99e962ee31ef4b5c7a93c7d5b23ebb373e19
Gennady Verdel <godrose@gmail.com>
2021-07-23 14:02:47 +0300

    devops: bump version

--- devops/update/update-all.bat
+++ devops/update/update-all.bat
@@ -1,4 +1,4 @@
 call update-single bump-dependency-version Solid 2.3.1
 call update-single bump-dependency-version Attest 2.4.0
-call update-single bump-version LogoFX 2.2.1-rc1
-call update-single bump-dependency-version LogoFX 2.2.1-rc1
+call update-single bump-version LogoFX 2.2.1
+call update-single bump-dependency-version LogoFX 2.2.1
